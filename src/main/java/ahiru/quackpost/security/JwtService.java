00633670f09307784ea1387b471f79473e6dd0a4
ThomasIsHere <lavier.thomas@gmail.com>
2024-03-01 09:21:54 +0100

    Extract one claim from all claims and extract usernam from claims

--- src/main/java/ahiru/quackpost/security/JwtService.java
+++ src/main/java/ahiru/quackpost/security/JwtService.java
@@ -7,6 +7,7 @@ import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
@@ -14,7 +15,12 @@ public class JwtService {
     private static final String SECRET_KEY = "609b4a5e6dd5d7e1d6cbe60cfdcfb3a4024fa3454a15c217cb4803ea78bcc00e";
 
     public String extractUsername(String token) {
-        return null;
+        return extractClaim(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaim(String token, Function<Claims, T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
     }
 
     private Claims extractAllClaims(String token){
