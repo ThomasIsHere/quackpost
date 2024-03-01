607a9c8a50e92a94c1e88dcfec3ee7fefb0ece1d
ThomasIsHere <lavier.thomas@gmail.com>
2024-03-01 23:04:16 +0100

    Update Security config with JwtAuthFilter, Session management, and authProvider

--- src/main/java/ahiru/quackpost/security/SecurityConfig.java
+++ src/main/java/ahiru/quackpost/security/SecurityConfig.java
@@ -1,17 +1,26 @@
 package ahiru.quackpost.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    //public static final String[] WHITE_LIST_URL = {"/quack_post"};
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    public static final String[] WHITE_LIST_URL = {"/quack_post"};
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,9 +28,17 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(
                         req ->
-                                //req.requestMatchers(WHITE_LIST_URL).permitAll()
-                                req.anyRequest().permitAll()
-                );
+                                req
+                                        .requestMatchers(WHITE_LIST_URL).permitAll()
+                                        .anyRequest().authenticated()
+                )
+                .sessionManagement(
+                        session -> session
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class)
+        ;
 
         return http.build();
     }
